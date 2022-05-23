f0804a6cde0c932c739336a9786440e6516c045b
Ekaterina Serova <eplisko884@gmail.com>
2022-05-23 17:13:50 +0300

    исправлены ошибки

--- Serova_Ekaterina_dz_1.py
+++ Serova_Ekaterina_dz_1.py
@@ -40,3 +40,73 @@ if one_day <= user_time:
     minute = user_time // one_minute
     second = user_time % one_minute
     print('{} дн {} час {} мин {} сек'.format(day, hour, minute, second))
+
+#2. Создать список, состоящий из кубов нечётных чисел от 1 до 1000 (куб X - третья степень числа X):
+#Вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7.
+# Например, число «19 ^ 3 = 6859» будем включать в сумму, так как 6 + 8 + 5 + 9 = 28 –
+# делится нацело на 7. Внимание: использовать только арифметические операции!
+#К каждому элементу списка добавить 17 и заново вычислить сумму тех чисел из этого списка,
+# сумма цифр которых делится нацело на 7.
+#* Решить задачу под пунктом b, не создавая новый список.
+
+import random
+
+list_cubes = [x**3 for x in range(1000) if  x%2 != 0 ]
+my_sum_list =[]
+
+for n in range(len(list_cubes)):
+    number_in_str = str(list_cubes[n])
+    list_sum=list(number_in_str) #разбиение числа на отдельные
+    #print(list_sum_number_in_str)
+    for i in range(len(list_sum)):
+        list_sum[i]=int(list_sum[i])
+    #print (list_sum)
+    sum_numbers = sum(list_sum) # сумма чисел
+    #print(sum_numbers)
+    if sum_numbers%7==0:
+        my_sum_list.append(list_cubes[n])
+print (sum(my_sum_list))
+
+list_cubes = [x**3+17 for x in range(1000) if  x%2 != 0 ]
+my_sum_list =[]
+
+for n in range(len(list_cubes)):
+    number_in_str = str(list_cubes[n])
+    list_sum=list(number_in_str) #разбиение числа на отдельные
+    #print(list_sum_number_in_str)
+    for i in range(len(list_sum)):
+        list_sum[i]=int(list_sum[i])
+    #print (list_sum)
+    sum_numbers = sum(list_sum) # сумма чисел
+    #print(sum_numbers)
+    if sum_numbers%7==0:
+        my_sum_list.append(list_cubes[n])
+print (sum(my_sum_list))
+
+#Склонение слова
+#Реализовать склонение слова «процент» во фразе «N процентов».
+#Вывести эту фразу на экран отдельной строкой для каждого из чисел в интервале от 1 до 100:
+#1 процент
+#2 процента
+#3 процента
+#4 процента
+#5 процентов
+#6 процентов..
+
+
+for i in range(1,101):
+    str_percent=str(i)
+    if str_percent[-1]=='1':
+        print('{} процент'.format(str_percent))
+    elif str_percent[-1]=='2' or str_percent[-1]=='3' or str_percent[-1]=='4':
+        print('{} процента'.format(str_percent))
+    else:
+        print('{} процентов'.format(str_percent))
+
+
+
+
+
+
+
+
