17dfe686aa3593ea1456fb5151d95ca0117068f6
Ekaterina Serova <eplisko884@gmail.com>
2022-05-29 20:16:56 +0300

    выполнено задание 3-4

--- Serova_Ek_dz_3.py
+++ Serova_Ek_dz_3.py
@@ -24,3 +24,30 @@ def num_translate():
         print ('None')
 
 num_translate()
+
+#задание 3-4
+
+def thesaurus_adv(*args):
+    result={}
+    for name_en in args:
+        name,surname = name_en.split(" ")
+        if result.get(surname[0]) == None:
+            result.setdefault(surname[0], {})
+            result.setdefault(surname[0],thesaurus(name,surname,result[surname[0]]))
+        else:
+            result[surname[0]] = thesaurus(name,surname,result[surname[0]])
+
+    print(result)
+
+def thesaurus(name_em,surname_em,result_thesaurus):
+    if result_thesaurus.get(name_em[0]) == None:
+        result_thesaurus.setdefault(name_em[0], [])
+        result_thesaurus[name_em[0]].append(f'{name_em} {surname_em}')
+    else:
+        result_thesaurus[name_em[0]].append(f'{name_em} {surname_em}')
+    return result_thesaurus
+
+thesaurus_adv("Иван Иванов", "Инна Серова", "Полина Алексеева", "Петр Алексеев", "Андрей Александров")
+
+
+
